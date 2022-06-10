180a5d5e3b85663db9e05d34a0d584651f32d626
Venkatmorningstar <venkatasalam.m@applogiq.org>
2022-06-10 18:38:46 +0530

    basic setup update(6/10/2022)

---
+++ app/common/images.js
@@ -0,0 +1,5 @@
+export const IMAGES = {
+  'eye-open' : require('../assets/images/eyeopen.png'),
+  'eye-closed' : require('../assets/images/eyeclosed.png'),
+   
+  };
--- app/components/inputComponent.js
+++ app/components/inputComponent.js
@@ -1,15 +1,17 @@
 //This is an inputfield with a title and input
  //It receives title,placeholder,value and callback function as props from parent
  //basic usage const handleInput = (childData) =>{setValue(childData)} 
- //<InputField parentCallback = {handleInput}></InputField>
+ //<InputField showicon={false} parentCallback={parentCallback1} title={"userpass"} value={val1} placeholder={"password"} ></InputField>
 /**************************************** Import Packages ***********************************************************/
-import React from "react";
-import {Text,View,StyleSheet,TextInput,useWindowDimensions} from "react-native";
+import React,{useState} from "react";
+import {Text,View,StyleSheet,TextInput,useWindowDimensions,Image,Pressable} from "react-native";
+import { IMAGES } from "../common/images";
 /**************************************** Import components ***********************************************************/
 
 
-const InputField =({parentCallback,placeholder,title,value})=>{
+const InputField =({parentCallback,placeholder,title,value,showicon})=>{
     const { height, width } = useWindowDimensions();
+    const [pressed,setPressed] = useState( showicon ? false : true)
     //Get height,width of screen by this hook
     var w = width
     var h = height*(10/100)
@@ -20,10 +22,21 @@ const    onchange = (value) => {
         parentCallback(value);
       
     }
+  const iconpress =()=>{
+      setPressed(!pressed)
+  }
     return(
         <View style={[styles.container,{height:h,width:w}]}>
-            <Text style={styles.title}>{title}</Text>
-<TextInput value={value} style={styles.input} placeholder={placeholder} onChangeText={(value)=>onchange(value)}></TextInput>
+            <Text  style={[styles.title,{textTransform:"capitalize"}]}>{title}</Text>
+<TextInput secureTextEntry={!pressed} value={value} style={styles.input} placeholder={placeholder} onChangeText={(value)=>onchange(value)}></TextInput>
+{showicon?   <View style={{position:"absolute",marginTop:height*(6.5/100),marginLeft:width*(85/100)}}>
+    <Pressable onPress={iconpress}>
+    <Image style={[styles.eyeicon]} source={pressed ? IMAGES["eye-open"] : IMAGES["eye-closed"]}></Image>
+    </Pressable>
+    </View>:null}
+ 
+   
+
         </View>
     )
 }
@@ -32,6 +45,7 @@ const styles = StyleSheet.create({
 marginTop:10,marginBottom:5
     },
     title:{color:"rgba(65, 39, 15, 0.8)",fontWeight:"600",fontFamily:"SourceSansPro-Bold",lineHeight:21,marginBottom:10},
-    input:{backgroundColor:"white",width:'100%',borderRadius:6}
+    input:{backgroundColor:"white",width:'100%',borderRadius:6},
+    eyeicon:{height:15,width:22}
 })
 export default InputField;
